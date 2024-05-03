9608a8b970ab090a8b6f25cc45c871b3772c71fc
John <emailjohnthomascaballero@gmail.com>
2024-05-03 22:02:34 +0800

    Day 8

---
+++ chapter-9-installing-nextjs/app/page.js
@@ -0,0 +1,33 @@
+// extjs uses React Server Components by default, thats why if you use client componenet put "use client" in your script.
+
+"use client";
+import { useState } from "react";
+
+function Header({ title }) {
+  return <h1>{title ? title : "Default title"}</h1>;
+}
+
+export default function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship." />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
+
+// the "dev" can change anything in package.json as you like.
