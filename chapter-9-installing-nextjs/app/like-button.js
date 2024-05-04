dd71fecbd9c8e912a1785d2c8d71efe6b936e805
John <emailjohnthomascaballero@gmail.com>
2024-05-04 22:11:30 +0800

    Day 9

---
+++ chapter-9-installing-nextjs/app/like-button.js
@@ -0,0 +1,13 @@
+// add the React 'use client' directive at the top of the file. This tells React to render the component on the client.
+
+'use client';
+import { useState } from "react";
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
--- chapter-9-installing-nextjs/app/page.js
+++ chapter-9-installing-nextjs/app/page.js
@@ -1,7 +1,6 @@
-// extjs uses React Server Components by default, thats why if you use client componenet put "use client" in your script.
+// nextjs uses React Server Components by default, thats why if you use client component put "use client" in your script.
 
-"use client";
-import { useState } from "react";
+import LikeButton from "./like-button";
 
 function Header({ title }) {
   return <h1>{title ? title : "Default title"}</h1>;
@@ -9,12 +8,6 @@ function Header({ title }) {
 
 export default function HomePage() {
   const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
-
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
 
   return (
     <div>
@@ -24,8 +17,7 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
-
-      <button onClick={handleClick}>Like ({likes})</button>
+      <LikeButton />
     </div>
   );
 }
